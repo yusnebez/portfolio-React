d28d901c6cd0aef115e7b9750cc6ddaea152965c
yusnebez <yusnebez@gmail>
2022-07-02 16:46:22 +0100

    sin movingtext

--- src/component/Presentation.jsx
+++ src/component/Presentation.jsx
@@ -1,5 +1,4 @@
 import {motion} from 'framer-motion'
-import MovingText from 'react-moving-text'
 import {AiOutlineDown} from 'react-icons/ai'
 
 export const Presentation = () => {
@@ -10,14 +9,8 @@ export const Presentation = () => {
   
     >
         <div  className='col-start-1 row-start-1 lg:row-start-2'>
-            <MovingText className='ml-4 text-2xl font-bold text-white mt-28 lg:mt-30 md:mt-24 lg:text-4xl md:ml-32 lg:ml-40 '
-                type="fadeInFromBottom"
-                duration="3000ms"
-                delay="0s"
-                direction="normal"
-                timing="ease"
-                iteration="1"
-                fillMode="none">
+            <div className='ml-4 text-2xl font-bold text-white mt-28 lg:mt-30 md:mt-24 lg:text-4xl md:ml-32 lg:ml-40 '
+                >
                 Desarrollador Full Stack 
                 <p className='pt-5 mr-8 text-xl font-light text-white'>
                     Autodidacta, apasionado por la 
@@ -28,31 +21,19 @@ export const Presentation = () => {
                 <button type='submit' className='px-3 py-1 mt-5 text-xl bg-[#6A98F0] hover:scale-110  rounded-sm
                 '>Hiring me</button>
                 </form>
-            </MovingText>
+            </div>
         </div>
         <div className='col-start-1 row-start-1'>
-            <MovingText className='lg:mt-80 mt-16 md:mt-8 lg:text-4xl  font-bold text-[#6A98F0] lg:ml-40 md:ml-32 pb-2 text-2xl ml-4'
-                type="fadeInFromBottom"
-                duration="3000ms"                                             vv  vc      
-                delay="0s"
-                direction="normal"
-                timing="ease"
-                iteration="1"
-                fillMode="none">
+            <div className='lg:mt-80 mt-16 md:mt-8 lg:text-4xl  font-bold text-[#6A98F0] lg:ml-40 md:ml-32 pb-2 text-2xl ml-4'
+                >
                 Zebensui Herrera
-            </MovingText>
+            </div>
         </div>
         <div className='hidden text-center lg:grid lg:row-start-5 md:col-span-2'>
-            <MovingText className='mt-24 ml-4 text-lg text-[#191919] md:text-white'
-                type="squeezeVertical"
-                duration="4000ms"
-                delay="0s"
-                direction="normal"
-                timing="ease"
-                iteration="4"
-                fillMode="none">
+            <div className='mt-24 ml-4 text-lg text-[#191919] md:text-white'
+                >
                 Scroll
-            </MovingText>
+            </div>
         </div>
         
         <div className='hidden col-start-2 row-start-6 lg:grid'>
