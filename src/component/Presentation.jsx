133d8e16c745178ca1f5d7c72d8dc014f7469aa8
yusnebez <yusnebez@gmail>
2022-07-03 01:21:04 +0100

    movimiento texto v4.0

--- src/component/Presentation.jsx
+++ src/component/Presentation.jsx
@@ -14,43 +14,41 @@ export const Presentation = () => {
   
     >
         <div  className='col-start-1 row-start-1 lg:row-start-2'>
-            <div className='ml-4 text-2xl font-bold text-white mt-28 lg:mt-30 md:mt-24 lg:text-4xl md:ml-32 lg:ml-40 '
+            <div className='mt-24 ml-4 text-2xl font-bold text-white lg:mt-30 md:mt-24 lg:text-4xl md:ml-32 lg:ml-40'
                 >
                 <div data-aos-duration="2000" data-aos="fade-right">
                     <p>Desarrollador Full Stack</p>
                 </div>
                 <div data-aos-duration='2000' data-aos="fade-up">
-                    <p className='pt-5 mr-8 text-xl font-light text-white'>
+                    <p className='pt-5 mb-2 mr-8 text-xl font-light text-white'>
                         Autodidacta, apasionado por la 
                         informática y la programación. Especializado en el 
                         stack MERN
                     </p>
                     
                     <form action='mailto:yusnebez@gmail.com'>
-                    <button type='submit' className='px-3 py-1 mt-5 text-xl bg-[#6A98F0] hover:scale-110  rounded-sm
+                    <button type='submit' className='px-3 py-1 mt-5 mb-4 text-xl bg-[#6A98F0] hover:scale-110  rounded-sm
                     '>Hiring me</button>
                     </form>
                 </div>
             </div>
         </div>
         <div className='col-start-1 row-start-1'>
-            <div data-aos-duration='2000' data-aos="fade-down" className='lg:mt-80 mt-16 md:mt-8 lg:text-4xl  font-bold text-[#6A98F0] lg:ml-40 md:ml-32 pb-2 text-2xl ml-4'
+            <div data-aos-duration='2000' data-aos="fade-down" className='lg:mt-80 mt-18 md:mt-8 lg:text-4xl  font-bold text-[#6A98F0] lg:ml-40 md:ml-32 pb-2 text-2xl ml-4'
                 >
                 Zebensui Herrera
             </div>
         </div>
         <div className='hidden text-center lg:grid lg:row-start-5 md:col-span-2'>
-            <div className='mt-24 ml-4 text-lg text-[#191919] md:text-white'
+            <div className='mt-24 mb-4 ml-8 text-lg text-[#191919] md:text-white'
                 >
                 Scroll
             </div>
         </div>
         
         <div className='hidden col-start-2 row-start-6 lg:grid'>
-            <button className='py-12 mb-1 ml-[0.4rem] border-l-2 border-white'></button>
-            <AiOutlineDown className='mb-2 mr-2 text-white' />
+            <AiOutlineDown className='mb-16 mr-2 text-3xl text-white' />
         </div>
-        
         
     </motion.div>
   )
