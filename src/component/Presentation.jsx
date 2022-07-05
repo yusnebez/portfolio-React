368a181ab4a8bb267a86cab732d07e9081a0a0a4
yusnebez <yusnebez@gmail>
2022-07-05 02:27:57 +0100

    con primer proyecto

--- src/component/Presentation.jsx
+++ src/component/Presentation.jsx
@@ -14,7 +14,7 @@ export const Presentation = () => {
   
     >
         <div  className='col-start-1 row-start-1 lg:row-start-2'>
-            <div className='mt-24 ml-4 text-2xl font-bold text-white lg:mt-30 md:mt-24 lg:text-4xl md:ml-32 lg:ml-40'
+            <div className='mt-24 ml-4 text-2xl font-bold text-white lg:-mt-16 md:mt-24 lg:text-4xl md:ml-32 lg:ml-40'
                 >
                 <div data-aos-duration="2000" data-aos="fade-right">
                     <p>Desarrollador Full Stack & Técnico de Sistemas</p>
@@ -33,7 +33,7 @@ export const Presentation = () => {
             </div>
         </div>
         <div className='col-start-1 row-start-1'>
-            <div data-aos-duration='2000' data-aos="fade-down" className='lg:mt-80 mt-16 md:mt-8 lg:text-4xl  font-bold text-[#6A98F0] lg:ml-40 md:ml-32 pb-2 text-2xl ml-4'
+            <div data-aos-duration='2000' data-aos="fade-down" className='lg:mt-40 mt-16 md:mt-8 lg:text-4xl  font-bold text-[#6A98F0] lg:ml-40 md:ml-32 pb-2 text-2xl ml-4'
                 >
                 Zebensui Herrera
             </div>
@@ -49,9 +49,9 @@ export const Presentation = () => {
         <div className='hidden col-start-2 row-start-6 lg:grid'>
             <AiOutlineDown  className='mb-16 mr-2 text-3xl text-white' />
         </div>
-        <div div data-aos-duration="3000" data-aos="fade-left" className='lg:ml-16 lg:mt-56 lg:col-start-2 lg:row-start-1'>
-            <img className='landscape:hidden md:portrait:hidden' src='https://res.cloudinary.com/zeben/image/upload/c_scale,w_1000/v1656929380/portfolio/Dise%C3%B1o_sin_t%C3%ADtulo_9_gc5zna.png'/>
-            <img className='xl:hidden sm:portrait:hidden md:portrait:grid md:portrait:ml-40 landscape:ml-40' src='https://res.cloudinary.com/zeben/image/upload/c_scale,w_500/v1656929380/portfolio/Dise%C3%B1o_sin_t%C3%ADtulo_9_gc5zna.png'/>
+        <div div data-aos-duration="3000" data-aos="fade-left" className='lg:ml-16 lg:mt-40 lg:col-start-2 lg:row-start-1'>
+            <img className='md:portrait:hidden' src='https://res.cloudinary.com/zeben/image/upload/c_scale,w_1000/v1656929380/portfolio/Dise%C3%B1o_sin_t%C3%ADtulo_9_gc5zna.png'/>
+            <img className='xl:hidden portrait:hidden md:portrait:grid md:portrait:ml-40 sm:landscape:ml-40 lg:landscape:hidden' src='https://res.cloudinary.com/zeben/image/upload/c_scale,w_500/v1656929380/portfolio/Dise%C3%B1o_sin_t%C3%ADtulo_9_gc5zna.png'/>
         </div>
         
     </motion.div>
--- src/component/Proyect.jsx
+++ src/component/Proyect.jsx
@@ -12,9 +12,10 @@ export const Proyect = () => {
     whileInView={{ opacity: 1 }}
     transition={{ duration: 1 }}
     >
+      
       <div className='grid grid-cols-1 grid-rows-3 gap-5 mt-5 ml-6 mr-6 lg:gap-10 xl:ml-40 lg:grid-rows-3 lg:ml-20'>
       <div className='grid gap-4 lg: lg:grid-cols-1 xl:grid-cols-2'>
-          <div className='grid col-start-1 lg:ml-4 p-4 text-white bg-[#232323] rounded-lg lg:h-[300px] md:w-[680px] xl:w-[600px] lg:w-[970px] md:ml-20'>
+          <div className='grid col-start-1 lg:ml-4 p-4 text-white bg-[#232323] rounded-lg lg:h-[300px] md:w-[680px] xl:landscape:w-[625px] lg:w-[970px] md:ml-20 lg:landscape:w-[1070px]'>
             <p className='mt-6'>
               2.Página web para gestión de villas, se listan las villas y se actualizan las 
               mismas. Se utiliza para el Front Reactjs con Context Api y Tailwind. Para el Back 
@@ -31,11 +32,11 @@ export const Proyect = () => {
            <button className='grid col-start-2 bg-[#6A98F0] text-white w-[60px] md:w-24 rounded-sm mt-8 px-2 ml-6 lg:pt-2'>Github</button>
           </div>
           </div>
-          <div className='xl:grid lg:col-start-2 bg-amber-200 hidden xl:h-[300px] rounded-lg xl:w-[600] col-span-3'>
+          <div className='xl:grid lg:col-start-2 bg-amber-200 hidden xl:h-[300px] rounded-lg xl:w-[625px] col-span-3'>
           </div>
         </div>
         <div className='grid row-start-2 gap-4 lg:grid-cols-2'>
-          <div className='grid col-start-1 lg:ml-4 p-4 text-white bg-[#232323] rounded-lg lg:h-[300px] lg:w-[970px] xl:w-[600px] md:w-[680px] md:ml-20'>
+          <div className='grid col-start-1 lg:ml-4 p-4 text-white bg-[#232323] rounded-lg lg:h-[300px] lg:w-[970px] xl:landscape:w-[625px] md:w-[680px] md:ml-20 lg:landscape:w-[1070px]'>
             <p className='mt-6'>
               2.Página web para gestión de villas, se listan las villas y se actualizan las 
               mismas. Se utiliza para el Front Reactjs con Context Api y Tailwind. Para el Back 
@@ -52,28 +53,27 @@ export const Proyect = () => {
            <button className='grid col-start-2 bg-[#6A98F0] text-white w-[60px] md:w-24 rounded-sm mt-8 px-2 ml-6 lg:pt-2'>Github</button>
           </div>
           </div>
-          <div className='xl:grid lg:col-start-2 bg-amber-200 xl:h-[300px] hidden rounded-lg xl:w-[600] col-span-3'>
+          <div className='xl:grid lg:col-start-2 bg-amber-200 xl:h-[300px] hidden rounded-lg xl:w-[625px] col-span-3'>
           </div>
         </div>
         <div className='grid row-start-3 gap-4 lg:grid-cols-1 xl:grid-cols-2'>
-          <div className='grid col-start-1 lg:ml-4 p-4 text-white bg-[#232323] rounded-lg lg:h-[300px] lg:w-[970px] xl:w-[600px] md:w-[680px] md:ml-20'>
+          <div className='grid col-start-1 lg:ml-4 p-4 text-white bg-[#232323] rounded-lg lg:h-[300px] lg:w-[970px] xl:landscape:w-[625px] md:w-[680px] md:ml-20  lg:landscape:w-[1070px]'>
             <p className='mt-6'>
-              2.Página web para gestión de villas, se listan las villas y se actualizan las 
-              mismas. Se utiliza para el Front Reactjs con Context Api y Tailwind. Para el Back 
-              NodeJs con MongoDB de base de datos.
+              El juego está hecho con Javascript y CSS, listo para usar en un navegador compatible con html5.
+              Basado en Final Frontier.
             </p>
           <div className='grid grid-rows-1 mt-10 gris-cols-4'>
-              <SiReact  className='grid col-start-1 text-2xl md:ml-20 ml-10 text-[#6A98F0]'/>
+              <SiReact  className='grid col-start-1 text-2xl md:ml-10 ml-10 text-[#6A98F0]'/>
               <SiTailwindcss className='grid col-start-2 text-2xl text-[#6A98F0]'/>
               <FaNodeJs className='grid col-start-3 text-2xl text-[#6A98F0]'/>
               <SiMongodb className='grid col-start-4 text-2xl text-[#6A98F0]'/>
           </div>
           <div className='grid grid-cols-5 grid-rows-1 gap-4 pt-4 ml-20 md:ml-40'>
-           <button className='grid col-start-1 bg-[#6A98F0] text-white w-[60px] md:w-24 rounded-sm mt-8 px-2 lg:pt-2'>Visitar</button>
-           <button className='grid col-start-2 bg-[#6A98F0] text-white w-[60px] md:w-24 rounded-sm mt-8 px-2 ml-6 lg:pt-2'>Github</button>
+           <a href="https://yusnebez.github.io/ScrollVerticalGame/" className='grid col-start-1 bg-[#6A98F0] text-white w-[50px] md:w-24 rounded-sm mt-8 pl-6 lg:pt-2'>Visitar</a>
+           <a href="https://github.com/yusnebez/ScrollVerticalGame" className='grid col-start-2 bg-[#6A98F0] text-white w-[50px] md:w-24 rounded-sm mt-8 pl-6 ml-6 lg:pt-2'>Github</a>
           </div>
           </div>
-          <div className='xl:grid lg:col-start-2 bg-amber-200 hidden xl:h-[300px] rounded-lg xl:w-[600] col-span-3'>
+          <div className='xl:grid lg:col-start-2 bg-amber-200 hidden xl:h-[300px] rounded-lg xl:w-[625px] col-span-3'>
           </div>
         </div>
 
